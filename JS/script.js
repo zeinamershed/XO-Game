be0814dc709980e0e4b9a42877e8a1ecfb84ed1a
Zeina <zeinamershed1@gmail.com>
2024-05-22 13:53:42 +0200

    done

---
+++ JS/script.js
@@ -0,0 +1,92 @@
+const startpage = document.getElementById('startpage');
+const gamepage = document.getElementById('gamepage');
+
+let title = document.querySelector('.title');
+let turn = 'x';
+let squares = [];
+
+document.getElementById('startBtn').addEventListener('click', function() {
+    startpage.style.display='none';
+    gamepage.style.display='block';
+});
+
+function end(num1,num2,num3) {
+    title.innerHTML = `${squares[num1]} winner`;
+    document.getElementById('item'+num1).style.background = '#217980';
+    document.getElementById('item'+num2).style.background = '#217980';
+    document.getElementById('item'+num3).style.background = '#217980';
+    document.getElementById('winnerSound').play();
+
+    setInterval(function(){title.innerHTML += '.'},1000);
+    setTimeout(function(){location.reload()},4000)
+}
+function winner() {
+    for(let i = 1; i < 10; i++) {
+        squares[i] = document.getElementById('item' + i).innerHTML;
+    }
+    if(squares[1] == squares[2] && squares[2] == squares[3]  && squares[1] != '') {
+        end(1,2,3);
+    } 
+    else if(squares[4] == squares[5] && squares[5] == squares[6]  && squares[5] != '') {
+        end(4,5,6);
+    }
+    else if(squares[7] == squares[8] && squares[8] == squares[9]  && squares[8] != '') {
+        end(7,8,9);
+    }
+
+
+    else if(squares[1] == squares[4] && squares[4] == squares[7]  && squares[1] != '') {
+        end(1,4,7);
+
+    }
+    else if(squares[2] == squares[5] && squares[5] == squares[8]  && squares[5] != '') {
+        end(2,5,8);
+
+    }
+    else if(squares[3] == squares[6] && squares[6] == squares[9]  && squares[6] != '') {
+        end(3,6,9);
+
+    }
+    else if(squares[1] == squares[5] && squares[5] == squares[9]  && squares[5] != '') {
+        end(1,5,9);
+    }
+    else if(squares[3] == squares[5] && squares[5] == squares[7]  && squares[5] != '') {
+        end(3,5,7);
+    }
+    if (squares.every(cell => cell !== '')) {
+           // title.innerHTML = `${squares['num1']} winner`;
+           // document.getElementById('winnerSound').play(); 
+            //setTimeout(function(){location.reload()},4000);
+            loser();
+    } 
+}
+
+function loser() {
+    //if (squares.every(cell => cell !== '')) {
+        //if (!winner()) { 
+            title.innerHTML = "Game Over!";
+            document.getElementById('loserSound').play(); 
+            setTimeout(function(){location.reload()},4000);
+       // }
+        //return true;
+    //}
+    //return false;
+}
+
+function game(id) {
+    let element = document.getElementById(id);
+    if(turn === 'x' && element.innerHTML == '') {
+        element.innerHTML = 'x';
+        turn = 'o';
+        title.innerHTML = 'O'; 
+    }
+    else if(turn === 'o' && element.innerHTML == '') {
+        element.innerHTML = 'O';
+        turn = 'x';
+        title.innerHTML = 'X';
+    }
+    
+    winner();
+   
+   
+}
---
+++ README.md
@@ -0,0 +1,53 @@
+
+## Overview
+
+This is a simple Tic Tac Toe game implemented in HTML, CSS, and JavaScript. It allows two players to play against each other on a 3x3 grid.
+
+## Features
+
+- Responsive design
+- Basic game logic to determine win/draw conditions
+- Sound effects for winner and loser
+- Restart functionality
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Setup
+
+1. Clone the repository:
+
+```bash
+git clone https://https://github.com/zeinamershed/tic-tac-toe.git
+Open index.html in your web browser.
+How to Play
+Click on an empty cell to place your symbol (X or O).
+The game will automatically determine if a player has won or if it's a draw.
+Click the "Restart" button to start a new game.
+Folder Structure
+Code kopieren
+tic-tac-toe/
+│
+├── index.html
+├── styles.css
+├── script.js
+├── winner.mp3
+├── loser.mp3
+└── README.md
+Demo
+You can play the game here.
+
+Credits
+Sounds obtained from FreeSound
+License
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+vbnet
+Code kopieren
+
+Customize the content of the `README.md` file with relevant information about your game, such as how to set it up, how to play, any dependencies, credits, license information, etc.
+
+Once you've created the `README.md` file, commit it to your GitHub repository along with your other project files. This will provide a helpful guide for users and contributors to understand and use your game.
